f961d3a7ac429d285b50ab32162e05692923913e
General090 <gospelkalu33@gmail.com>
2025-06-13 16:44:39 +0100

    updated

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ProfileCard(props) {
   return (
-    <div className='md:w-[350px] h-[30rem] p-[40px_20px] lg:my-[50px] my-[50%] mx-3 md:mx-auto bg-white rounded-[15px] text-center font-sans'>
+    <div className='md:w-[350px] h-[30rem] p-[40px_20px] lg:my-[50px] my-[10%] mx-3 md:mx-auto bg-white rounded-[15px] text-center font-sans'>
       <img src={props.img} alt={props.heading} className="w-[200px] h-[200px] mx-auto object-cover rounded-full mb-[15px]" />
       <h1 className='text-[35px] my-[10px] font-bold text-[#333]'>{props.heading}</h1>
       <p className='text-[16px] text-[#666]'>{props.bio}</p>
